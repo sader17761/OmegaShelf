34479facabc88a55039fb9cdaafbada699bb6732
Corey Sader <corey.sader@gmail.com>
2017-08-13 12:03:19 -0500

    added mongodb connection

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,7 @@
 var mongoose = require( 'mongoose' );
 
-mongoose.connect( 'localhost:27017/OmegaShelf' );
+mongoose.connect('mongodb://heroku_g719pp6x:hl47s24rif9k8mr115os279has@ds155718.mlab.com:55718/heroku_g719pp6x');
+//localhost:27017/OmegaShelf
 
 var userSchema = new mongoose.Schema({
   username: String,
